fedf0421b06bd58761b7b925e23e68929a557587
Astitvazz <153696739+Astitvazz@users.noreply.github.com>
2026-06-06 23:49:16 +0530

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool helper(vector<vector<char>>& board, string word, int i, int j,int k,int m, int n,vector<vector<bool>>& visited){
+        //base case
+        if(k>=word.size()){
+            return true;
+        }
+        if(i>=m||i<0||j>=n||j<0){
+            return false;
+        }
+        if(board[i][j]!=word[k]){
+            return false;
+        }
+        if(visited[i][j]){
+            return false;
+        }
+        visited[i][j]=true;
+        //recursive code
+        bool a=0;
+        bool b=0;
+        bool c=0;
+        bool d=0;
+  
+            a=helper(board,word,i+1,j,k+1,m,n,visited);
+       
+       
+            b=helper(board,word,i-1,j,k+1,m,n,visited);
+        
+   
+            c=helper(board,word,i,j+1,k+1,m,n,visited);
+        
+
+            d=helper(board,word,i,j-1,k+1,m,n,visited);
+        visited[i][j]=false;
+        return a||b||c||d;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int m=board.size();
+        int n=board[0].size();
+        bool res=0;
+        int k=0;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                vector<vector<bool>> visited(m,vector<bool>(n,0));
+                res=res||helper(board,word,i,j,k,m,n,visited);
+            }
+        }
+        return res;
+    }
+};
